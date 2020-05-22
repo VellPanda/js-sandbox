99a42410dbc4035545ad4c662f429bfac14493af
VellPanda <zagaikanvasile@gmail.com>
2020-05-22 18:24:13 +0100

    Added DataTypes.js

---
+++ js/datatypes.js
@@ -0,0 +1,29 @@
+// PRIMITIVE DATA TYPES
+
+// String
+const name = 'Alan Smith';
+// Number
+const age = 34;
+// Boolean
+const hasCar = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol(); // ES6
+
+// REFERENCE DATA TYPES - OBJECTS
+
+// Array 
+const tools = ['drill', 'hammer'];
+// Object literal
+const address = {
+  city: 'London',
+  postCode: 'K57VK8'
+}
+
+// Date 
+const today = new Date();
+console.log(today);
+console.log(typeof today);
