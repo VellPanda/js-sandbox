f92491c540013ae3cf983ebb9b3c53a1c7b2f067
VellPanda <zagaikanvasile@gmail.com>
2020-05-26 09:48:08 +0100

    Added MathOperators

---
+++ js/mathoperators.js
@@ -0,0 +1,28 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+//Simple math with numbers 
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+//Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(3.1);
+val = Math.floor(5.9);
+val = Math.sqrt(64);
+val = Math.abs(-7);
+val = Math.pow(8, 2);
+val = Math.min(1,3,5,6,95,-1,9);
+val = Math.max(1,3,5,6,95,-1,9);
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1); 
+
+
+console.log(val);
